0fb3d06870e8191e2fae5d85d269d8eaccdbeb26
rlukassa <19623069@std.stei.itb.ac.id>
2025-05-13 21:59:18 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base : '/Tubes2_masih-kuat-kok-Taylor-s-version-/',
   plugins: [react()],
 })
